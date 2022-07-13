ed2d0ca9dce0af3040ce401ec57357291a57d15c
MateuszPiotrowski1 <mt.piotrowski@o2.pl>
2022-07-13 20:13:47 +0200

    add discovering letters and word hiding functions

--- functions_hangman.py
+++ functions_hangman.py
@@ -9,3 +9,23 @@ def get_word():
         new_list.append(split_country[1])
     word = (random.choice(new_list))
     return word.upper()
+
+def word_hiding(word):
+    password = ''
+    for letter in word:
+        if letter != ' ':
+            letter = '_'
+            password += letter
+        elif letter == ' ':
+            password += letter
+    return list(password)
+
+def discovering_letters(discover_password):
+    discover_password_list = list(discover_password)
+    choice = input('')
+    hidden_word_list = word_hiding(discover_password)
+    for index,element in enumerate(discover_password_list):
+        if discover_password_list[index] == choice:
+            hidden_word_list[index] = choice
+    hidden_word = ''.join(hidden_word_list)
+    print(hidden_word)
